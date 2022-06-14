1cc6096515fd3b6e7a4cdf0650c44dd8f4b97b74
nsharp96 <62970403+nsharp96@users.noreply.github.com>
2022-06-14 23:54:05 +0100

    Layout and Pages

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    //Properties
+    //Hooks
+    //Context
+    //Methods
+    //View
+
+    return (
+        <h1>Home</h1>
+    );
+}
+
+export default Home;
---
+++ src/components/pages/Signin.js
@@ -0,0 +1,18 @@
+const Signin = () => {
+    
+    //Properties
+
+    //Hooks
+
+    //Context
+
+    //Methods
+
+    //Views
+
+    return (
+        <h1>Sign in</h1>
+    );
+}
+
+export default Signin;
